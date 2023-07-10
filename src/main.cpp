c21b8a92c97f967a37f68e9dbdecbbab919b586f
eliott23 <62487535+eliott23@users.noreply.github.com>
2023-07-10 12:38:10 +0100

    s

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,4 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <iostream>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <string>
-#include <sstream>
-#include <fstream>
+#include "rheaders.hpp"
 
 #define PORT 8080
 size_t  ffstrlen(const char *str)
---
+++ src/rheaders.hpp
@@ -0,0 +1,20 @@
+
+# pragma once
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+
+// size_t  ffstrlen(const char *str);
